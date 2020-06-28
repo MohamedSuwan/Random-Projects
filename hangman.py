05f64a5a3fbbb7a34743570efd9bd7bc9a110439
Mohamed Suwan <67264950+Royswan@users.noreply.github.com>
2020-06-28 19:11:11 +0300

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -14,17 +14,25 @@ c="""
         /   \\
        *     *
 """
-guess="p"
-word="wioord"
+word="hehehehe"
 wdic={i:word.__getitem__(i) for i in range (len(word))}
 wdisplay={i:"*" for i in range (len(word))}
-
+guesses=[]
 #word=[word[x] for x in range (len(word))]
-if guess not in wdic.values():
-  k=c.split("\n")
-  print([k[x] for x in range (5)])
-else:
-  for x in wdic.keys(): 
-    if wdic[x]==guess:
-      wdisplay[x]=wdic[x] 
-  print("".join(wdisplay.values()))
+inc=4
+for i in range(8):
+  guess=str(input("enter your guess "))
+
+  if guess not in wdic.values():
+    k=c.split("\n")
+    guesses.append(guess)
+    print("you tried: ",guesses)
+    #print([f"{k[x]}" for x in range (5)])
+    print("\n".join(k[0:inc]))
+    inc+=4
+  else:
+    for x in wdic.keys(): 
+      if wdic[x]==guess:
+        wdisplay[x]=wdic[x] 
+    print("".join(wdisplay.values()))
+    print("you tried: ",guesses)
