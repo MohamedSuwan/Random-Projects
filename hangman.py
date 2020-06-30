9b56a44cc9ffcdaaf22fb35e79a1900f9037dd9a
Mohamed Suwan <67264950+Royswan@users.noreply.github.com>
2020-06-30 21:51:42 +0300

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,5 +1,4 @@
-# here im gonna draw the hanged man character
-c="""
+hanged="""
           |
           |
         __|__
@@ -12,27 +11,35 @@ c="""
           |
          / \\
         /   \\
-       *     *
+       /     \\
+      *       *
 """
+hanged=hanged.split("\n")
 word="hehehehe"
-wdic={i:word.__getitem__(i) for i in range (len(word))}
+wdic={i:j for i,j in enumerate(word)}
 wdisplay={i:"*" for i in range (len(word))}
+print(f"Guess the word, it's {len(word)} characters long!","\n")
 guesses=[]
-#word=[word[x] for x in range (len(word))]
-inc=4
-for i in range(8):
-  guess=str(input("enter your guess "))
-
-  if guess not in wdic.values():
-    k=c.split("\n")
+inc=0
+for i in range(8+len(word)):
+    guess=str(input("Enter your guess: "))
     guesses.append(guess)
-    print("you tried: ",guesses)
-    #print([f"{k[x]}" for x in range (5)])
-    print("\n".join(k[0:inc]))
-    inc+=4
-  else:
-    for x in wdic.keys(): 
-      if wdic[x]==guess:
-        wdisplay[x]=wdic[x] 
-    print("".join(wdisplay.values()))
-    print("you tried: ",guesses)
+
+    if guess not in wdic.values():
+        inc+=2
+        if inc==18:
+            print("Sorry you lost! the word was: ",word)
+            break
+
+    else:
+        for x in wdic.keys(): 
+            if wdic[x]==guess:
+                wdisplay[x]=guess 
+
+    if wdic==wdisplay:
+        print("Correct the word is: ",word)
+        break
+
+    print("\n".join(hanged[0:inc]),"\n")
+    print(f"The word is: {''.join(wdisplay.values())}")
+    print(f"You tried: {guesses}")
